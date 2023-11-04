cc1a29a3c1ee232f3e79b02532aa8ba9a622a486
Muwonge Hassan Saava <hassansaava@gmail.com>
2023-11-04 23:05:43 +0300

    setup login form ui

---
+++ PersonalTracking/FrmLogin.cs
@@ -0,0 +1,18 @@
+namespace PersonalTracking
+{
+    public partial class FrmLogin : Form
+    {
+        public FrmLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void txtUserNo_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
